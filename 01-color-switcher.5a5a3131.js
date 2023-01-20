c82f772115cb400459ad6f9ff2274505b3c7d0e4
HusarOlha <111289699+HusarOlha@users.noreply.github.com>
2023-01-20 13:30:46 +0000

    Deploying to gh-pages from @ HusarOlha/goit-js-hw-09@e2c30ea50dda9f3bbaad5860f86314a26851347c 🚀

--- 01-color-switcher.5a5a3131.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(function(){a=setInterval((e="#".concat(Math.floor(16777215*Math.random()).toString(16)),void(document.body.style.backgroundColor=e)),1e3),t.start.disabled=!0,t.stop.disabled=!1;var e})),t.stop.addEventListener("click",(function(){clearInterval(a),t.start.disabled=!1,t.stop.disabled=!0}));var a=null}();
-//# sourceMappingURL=01-color-switcher.5a5a3131.js.map
